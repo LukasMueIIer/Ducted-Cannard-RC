826280b15a04a66a4019de1ec2737d306782ef83
LukasMueIIer <62990034+LukasMueIIer@users.noreply.github.com>
2024-01-25 11:05:42 +0100

    Worked on the optimizer
    Seems liek the buildup method is to non linear and empirical, the solver tries to exploit weaknesses in the equations and produces unphysical resulst soooooo .... gonna try lifting line and use this either for minor tweaking or for performance checks

--- Calculations/AircraftCalculationsTest.py
+++ Calculations/AircraftCalculationsTest.py
@@ -18,14 +18,56 @@ def createPlane(c0,c1,c2,x1,x2):
 
     return  asb.Airplane(name="TestPlane",xyz_ref=[0,0,0],wings=[main_wing],fuselages=[fuselage])
 
-plane = createPlane(0.5,0.3,0.1,[0,1,0],[0,2,0])
+opt = asb.Opti()
 
-plane.draw_three_view()
+targetLift = 90
 
-aeroTest = asb.AeroBuildup(
-    airplane=plane,
+#optimize wing geometry at CL of
+alphaOPT = 5
+
+#wing geometry
+c0 = opt.variable(init_guess=0.5)
+c1 = opt.variable(init_guess=0.5)
+c2 = opt.variable(init_guess=0.5)
+
+x1 = opt.variable(init_guess=[0,1,0])
+x2 = opt.variable(init_guess=[0,2,0])
+
+#plane = createPlane(0.5,0.5,0.5,[0,1,0],[0,2,0])
+
+#aero = asb.AeroBuildup(
+ #   airplane=plane,
+  #  op_point=asb.OperatingPoint(
+   #     velocity=10,
+    #    alpha=4,
+    #),
+#).run()
+
+
+
+#enforce geometric restrictions
+opt.subject_to(x1[1] >= 0.05)
+opt.subject_to(x2[1] > x1)
+
+opt.subject_to(x1[2] == 0)
+opt.subject_to(x2[2] == 0)
+
+
+planeOPT = createPlane(c0,c1,c2,x1,x2)
+
+aero = asb.AeroBuildup(
+    airplane=planeOPT,
     op_point=asb.OperatingPoint(
         velocity=10,
-        alpha=5,
+        alpha=alphaOPT,
     ),
 ).run()
+
+#opt.subject_to(aero["L"] == targetLift)
+opt.minimize(aero["CD"])
+
+res = opt.solve()
+
+plane = createPlane(res.value(c0),res.value(c1),res.value(c2),res.value(x1),res.value(x2))
+
+plane.draw_three_view()
