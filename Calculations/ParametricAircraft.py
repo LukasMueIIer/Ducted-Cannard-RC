9d00d632c05744c272a72d81a37ce61d5ccece34
LukasMueIIer <Mueller.Lukas@outlook.com>
2024-01-26 16:12:23 +0100

    LETS COMMINGG TO CHAGE THE DEVICEEE

--- Calculations/ParametricAircraft.py
+++ Calculations/ParametricAircraft.py
@@ -8,19 +8,24 @@ class OptimizableWing:  #a subclass that makes optimization and mass management 
         self.name = name
         self.n = sections
         
+        log = False
+
         #changing section to span, chords, sweep and dihedral(as angle)
-        self.span = opt.variable(init_guess = np.ones(sections) * iniSpan / sections)  
-        self.chord = opt.variable(init_guess = np.ones(sections) * iniCord)
-        self.sweep = opt.variable(init_guess = np.zeros(sections))
-        self.dihedral = opt.variable(init_guess = np.zeros(sections))
-        self.twist = opt.variable(init_guess = np.zeros(sections))
+        self.span = opt.variable(init_guess = np.ones(sections - 1) * iniSpan / sections, log_transform = log)  
+        self.chord = opt.variable(init_guess = np.ones(sections) * iniCord, log_transform = log)
+        self.sweep = opt.variable(init_guess = np.zeros(sections - 1),scale = sweepLimit)
+        self.dihedral = opt.variable(init_guess = np.zeros(sections - 1), scale = dihedralLimit)
+        self.twist = opt.variable(init_guess = np.zeros(sections), scale = twistLimit)
 
         for i in range(0,sections):
-            opt.subject_to(self.span[i] > 0)
-            opt.subject_to(self.chord[i] > 0)
-            opt.subject_to(np.abs(self.sweep[i]) < sweepLimit)
-            opt.subject_to(np.abs(self.dihedral[i]) < dihedralLimit)
+            if(i > 0):
+                opt.subject_to(self.chord[i] <= self.chord[i-1])
+            if(i < sections - 1):
+                opt.subject_to(self.span[i] > 0)
+                opt.subject_to(np.abs(self.sweep[i]) < sweepLimit)
+                opt.subject_to(np.abs(self.dihedral[i]) < dihedralLimit)
             opt.subject_to(np.abs(self.twist[i]) < twistLimit)
+        opt.subject_to(self.chord[-1] > 0.05)
 
         #Airfoil Shapes
         self.XSec = []  
@@ -38,12 +43,13 @@ class OptimizableWing:  #a subclass that makes optimization and mass management 
                 y = 0
                 z = self.position[2]
             else:
-                x = x + 0.25 * chord[i - 1] + span[i] * np.sin(sweep[i]) - 0.25 * chord[i]
-                y = y + span[i] * np.cos(sweep[i])
-                z = z + span[i] * np.sin(dihedral[i])
+                x = x + 0.25 * chord[i - 1] + span[i-1] * np.sin(sweep[i-1] / 180 * np.pi) - 0.25 * chord[i]
+                y = y + span[i-1] * np.cos(sweep[i-1] / 180 * np.pi )
+                z = z + span[i-1] * np.sin(dihedral[i-1] / 180 * np.pi )
+            _c = chord[i]
             _t = twist[i]
             _foil = self.XSec[i]
-            Xsec.append(asb.WingXSec(xyz_le=_x,chord=_c,twist=_t,airfoil=_foil))
+            Xsec.append(asb.WingXSec(xyz_le=[x,y,z],chord=_c,twist=_t,airfoil=_foil))
 
         #create main wing
         wing = asb.Wing(name=self.name,symmetric=True,xsecs=Xsec)
@@ -81,9 +87,6 @@ class OptimizableWing:  #a subclass that makes optimization and mass management 
         return self.getMass(span,chord,sweep,dihedral,twist, SparFactor, AreaFactor)
         
     
-
-
-
 opt = asb.Opti()
 
 #parameters
@@ -94,13 +97,13 @@ alpha = 4 #opt.variable(init_guess = 1)
 #opt.subject_to(alpha < 5)
 #opt.subject_to(alpha > 0)
 
-main_wing = OptimizableWing(opt, name="Main Wing",position = [0, 0, 0], sections = 3, iniSpan = 1, iniCord = 0.1, twistLimit = 2 , sweepLimit = 5, dihedralLimit = 3)
+main_wing = OptimizableWing(opt, name="Main Wing",position = [0, 0, 0], sections = 3, iniSpan = 3, iniCord = 0.3, twistLimit = 2 , sweepLimit = 10, dihedralLimit = 3)
 
 
 #wings = [main_wing.getWing()]
 plane = asb.Airplane(name="TestPlane",xyz_ref=[0,0,0],wings=[main_wing.getWingOpt()],fuselages=[])
 
-m = m0 + main_wing.getMassOpt(2,1)
+m = m0 + main_wing.getMassOpt(5,1)
 lift = m * 9.81 
 
 vlm = asb.VortexLatticeMethod(
@@ -108,19 +111,24 @@ vlm = asb.VortexLatticeMethod(
     op_point=asb.OperatingPoint(
         velocity=v,  # m/s
         alpha=alpha,  # degree
-    ),spanwise_resolution=1,
+    ),spanwise_resolution=2,
     chordwise_resolution=8
 )
 
 aero = vlm.run()
 
 opt.subject_to(aero["L"] == lift)
-opt.minimize(aero["D"] + 0.01 * plane.s_ref)
+opt.minimize(aero["D"] + plane.s_ref)
 
-res = opt.solve()
+rIter = []
+
+#def cb():
+ #   rIter.append(opti.debug)
+
+res = opt.solve(verbose=False)
 
 plane = asb.Airplane(name="ResultPlane",xyz_ref=[0,0,0],wings=[main_wing.getWingEval(res)],fuselages=[])
-
+plane.draw_three_view()
 plane.draw_three_view()
 
 print(res)
